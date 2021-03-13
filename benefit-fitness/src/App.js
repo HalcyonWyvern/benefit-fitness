69d6f99a82103b20d2655d301eb8c32a7aa9c39e
Nick <ntedesco627@gmail.com>
2021-03-13 13:00:48 -0600

    3/13/21

--- benefit-fitness/src/App.js
+++ benefit-fitness/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/pages/Home';
+import Navbar2 from "./components/Navbar2";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar2 />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={Login} />
+          <Route path={'/home'} exact component={Home}/>
         </Switch>
       </Router>
 
--- benefit-fitness/src/components/pages/Home.js
+++ benefit-fitness/src/components/pages/Home.js
@@ -1,13 +1,28 @@
 import React from "react";
 import '../../App.css';
-import HeroSection from "../HeroSection";
+import './Home.css';
+import Navbar2 from "../Navbar2";
 
 function Home ()
 {
     return (
-        <>
-            <HeroSection />
-        </>
+        <div>
+            {/*<Navbar2 />*/}
+        <div className='hero-container'>
+
+                {/*<img src='./images/BenefitBackground.png' />*/}
+                {/*<h1>Training</h1>*/}
+                {/*<p>What type of exercises?</p>*/}
+                {/*<div className="hero-btns">*/}
+                {/*    <Button className='btns' buttonStyle='btn--outline'*/}
+                {/*    buttonSize='btn--large'>GET STARTED*/}
+                {/*    </Button>*/}
+                {/*    <Button className='btns' buttonStyle='btn--primary'*/}
+                {/*            buttonSize='btn--large'>GET STARTED*/}
+                {/*    </Button>*/}
+                {/*</div>*/}
+        </div>
+        </div>
     )
 }
 
