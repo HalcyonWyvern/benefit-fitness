8289078b00b2a12ffb43a8440707dcbff5c3c744
HalcyonWyvern <cyclazocine@gmail.com>
2021-03-29 00:56:43 -0500

    Condenses down the contact page.

--- benefit-fitness/src/components/pages/Contact.js
+++ benefit-fitness/src/components/pages/Contact.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
-import {Button, Col, Container, Form, Jumbotron, Row} from "react-bootstrap";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
+
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { sendRequest } from "../../actions/Contact/contactActions"
+import axios from "axios";
 
 
 class Contact extends Component {
@@ -15,104 +16,93 @@ class Contact extends Component {
             requestedDate: "",
             comments: "",
             contactMethod: "",
-            errors: {}
         };
     }
 
     onSubmit = e => {
         const newRequest = {
-            user: this.state.user,
+            user: this.auth.user.id,
             phoneNumber: this.state.phoneNumber,
             requestType: this.state.requestType,
             requestedDate: this.state.requestedDate,
             comments: this.state.comments,
             contactMethod: this.state.contactMethod,
         };
-        this.props.sendRequest(newRequest)
+       axios
+           .post("/api/requests", newRequest)
+           .then(res => console.log(res.data));
     }
 
     render() {
         const {user} = this.props.auth;
-
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <p>{' '}</p>
-                        <Jumbotron>
-                            <h1>Contact Us!</h1>
-                            <p>
-                                <h6>We're more than willing to hear your personal requests.</h6>
-                                Please fill out the form below and one of our team members will reach out to you within
-                                five to ten business days.
-                            </p>
-                            <p>
-                                <Button variant="primary">Learn more</Button>
-                            </p>
-                        </Jumbotron>
-                    </Col>
-                </Row>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control id="phoneNumber" type="phoneNumber" placeholder="Phone" />
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>{' '}</p>
+                            <h2>Contact Us!</h2>
+                            <h6>Please Provide us with some basic information and we will reach out to you shortly.</h6>
 
-                        <Form.Text className="text-muted">
-                            We'll never share your phone or email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+                        </Col>
+                    </Row>
+                    <Form onSubmit={this.onSubmit}>
+                        <Form.Group>
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control id="phoneNumber" type="phoneNumber" placeholder="Phone"/>
 
-                    <Form.Group>
-                        <Form.Label>Request Type</Form.Label>
-                        <Form.Control as="select" multiple id="requestType">
-                            <option>Fitness Inquiry</option>
-                            <option>Request Exercise Plan</option>
-                            <option>Group Inquiry</option>
-                            <option>Speak to a Trainer</option>
-                            <option>Misc. Questions</option>
-                        </Form.Control>
-                    </Form.Group>
+                            <Form.Text className="text-muted">
+                                We'll never share your phone or email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>Contact Method</Form.Label>
-                        <Form.Control as="select" id="contactMethod">
-                            <option>Phone</option>
-                            <option>Email</option>
-                        </Form.Control>
-                    </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Request Type</Form.Label>
+                            <Form.Control as="select" multiple id="requestType">
+                                <option>Fitness Inquiry</option>
+                                <option>Request Exercise Plan</option>
+                                <option>Group Inquiry</option>
+                                <option>Speak to a Trainer</option>
+                                <option>Misc. Questions</option>
+                            </Form.Control>
+                        </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>Comments</Form.Label>
-                        <Form.Control id="comments" as="textarea" rows={3} />
-                    </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Contact Method</Form.Label>
+                            <Form.Control as="select" id="contactMethod">
+                                <option>Phone</option>
+                                <option>Email</option>
+                            </Form.Control>
+                        </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>Requested Date</Form.Label>
-                        <Form.Control id="requestedDate" type="date" placeholder="Date" />
-                    </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Details</Form.Label>
+                            <Form.Control id="comments" as="textarea" rows={3}/>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Requested Date</Form.Label>
+                            <Form.Control id="requestedDate" type="date" placeholder="Date"/>
+                        </Form.Group>
 
 
-
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </Container>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Container>
         );
     }
 }
 
 Contact.propTypes = {
-    sendRequest: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+    auth: state.auth
 });
 
 export default connect(
     mapStateToProps,
-    { sendRequest }
+    {}
 )(Contact);
