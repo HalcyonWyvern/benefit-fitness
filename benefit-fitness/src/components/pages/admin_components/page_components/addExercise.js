0150725186247e0946dcd3931188bf630be30753
HalcyonWyvern <cyclazocine@gmail.com>
2021-04-03 23:46:15 -0500

    Profile page now displays user information.

--- benefit-fitness/src/components/pages/admin_components/page_components/addExercise.js
+++ benefit-fitness/src/components/pages/admin_components/page_components/addExercise.js
@@ -1,1 +1,10 @@
 //Modal button for adding exercise to the library
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {Component} from "react";
+import {Button} from "react-bootstrap";
+
+class AddExercise extends Component {
+
+
+}
