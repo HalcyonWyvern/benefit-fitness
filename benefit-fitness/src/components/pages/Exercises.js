bcc62e31806bf0c6e15d02e084366e200c2ffd56
Nick <ntedesco627@gmail.com>
2021-04-07 01:54:48 -0500

    Added pagination and search for Exercise Table

--- benefit-fitness/src/components/pages/Exercises.js
+++ benefit-fitness/src/components/pages/Exercises.js
@@ -1,9 +1,8 @@
-import React, {Component, useState} from "react";
-import {Button, Container, Pagination, Row, Table, PaginationProps, PageItem} from "react-bootstrap";
+import React, {Component, useEffect, useMemo, useState} from "react";
+import { Container, Row, Table} from "react-bootstrap";
 import axios from "axios";
-import {render} from "@testing-library/react";
-import {Link} from "react-router-dom";
-import {func} from "prop-types";
+import PaginationComponent from "./page_components/PaginationComponent"
+import Search from "./page_components/Search";
 
 
 class Exercises extends Component {
@@ -19,14 +18,12 @@ class Exercises extends Component {
             videoURL: "",
             instructions: "",
             choices: [],
-            // isOpen: false,
             errors: {}
         };
     }
 
     componentDidMount = () => {
         this.getExercise();
-        this.clickHandler();
     }
 
     getExercise = () => {
@@ -42,149 +39,14 @@ class Exercises extends Component {
             });
     }
 
-
-    // handleChange = ({ target }) => {
-    //     const { name, value } = target;
-    //     this.setState({ [name]: value });
-    // };
-
-    clickHandler = (e) => {
-
-        this.setState({clicks: e})
-        console.log({clicks: e})
-
-    }
-
-
-
-        // choices.map(exercise =>
-        // <div key={exercise._id}>
-        //     <h3>{exercise.exerciseName}</h3>
-        //     <p>Instructions: {exercise.instructions}</p>
-        //     <p>Equipment: {exercise.equipment}</p>
-        //     <p>Sets: {exercise.sets}</p>
-        //     <p>Reps: {exercise.reps}</p>
-        //     <p>Exercise Type: {exercise.exerciseType}</p>
-        //     <p>Video Link: <a target="_blank" href={exercise.videoURL}>{exercise.videoURL}</a></p>
-        // </div>)
-
-    //
-
-    showModal = (exercise) => {
-        this.setState({
-            isOpen: true
-        })
-        console.log({clicks: exercise})
-    }
-
-    hideModal = () => {
-        this.setState({
-            isOpen: false
-        })
-    }
-
-
-
-    displayExercises = (choices) => {
-        // const pages =[5, 10, 25]
-        // const [page,setPage] = useState(0)
-        // const [rowsPerPage, setRowsPerPage] = useState(pages[page])
-
-        if (!choices.length) return null;
-
-        // const pages = [5, 10, 25]
-        // const [page, setPage] = useState(0)
-        // const [rowsPerPage, setRowsPerPage] = useState(pages[page])
-        //
-        // const tblPagination = () => {
-        //     const component = "div"
-        //     const page = [page]
-        //     const rowsPerPageOptions = [pages]
-        //     const rowsPerPage = [rowsPerPage]
-        //     const count = [choices.length]
-        //
-        // }
-
-        let active = 1;
-        let items = [];
-        for (let number = 1; number <= 5; number++) {
-            items.push(
-                <Pagination.Ellipsis key={number} active={number === active}>
-                    {number}
-                </Pagination.Ellipsis>,
-            );
-        }
-
-        return (
-            <div>
-                <Table striped bordered hover>
-                    <thead>
-                    <tr>
-                        <th>Exercise Name</th>
-                        <th>Exercise Type</th>
-                        <th>Equipment</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    {choices.map((exercise, index) =>
-                        <>
-                        <tr key={index}>
-                            <td onClick={this.showModal}>{exercise.exerciseName}</td>
-                            <td>{exercise.exerciseType}</td>
-                            <td>{exercise.equipment}</td>
-                        </tr>
-
-                        </>
-                    )}
-
-
-                    </tbody>
-                    <Pagination>{items}</Pagination>
-                </Table>
-
-                {/*<div>*/}
-                {/*    {clicks.map(data =>*/}
-                {/*    <h3>Name: {data.exerciseName}</h3>*/}
-                {/*    )}*/}
-                {/*</div>*/}
-                {/*{this.state.choices.map((exercise, index) =>*/}
-                {/*<>*/}
-                {/*    <div key={index}>*/}
-                {/*        <p>{exercise.exerciseName}</p>*/}
-                {/*    </div>*/}
-                {/*</> )}*/}
-                {/*{this.state.choices.map(exercise =>(*/}
-                {/*    <div key={exercise._id}>*/}
-
-                {/*    </div>*/}
-                {/*))}*/}
-                {/*{choices.map(exercise =>*/}
-                {/*    <>*/}
-                {/*    <h3>{exercise.exerciseName}</h3>*/}
-                {/*    <p>Instructions: {exercise.instructions}</p>*/}
-                {/*    <p>Equipment: {exercise.equipment}</p>*/}
-                {/*    <p>Sets: {exercise.sets}</p>*/}
-                {/*    <p>Reps: {exercise.reps}</p>*/}
-                {/*    <p>Exercise Type: {exercise.exerciseType}</p>*/}
-                {/*    <p>Video Link: <a target="_blank" href={exercise.videoURL}>{exercise.videoURL}</a></p>*/}
-                {/*    </>*/}
-                {/*)}*/}
-
-
-            </div>
-
-        );
-    };
-
     render() {
         return (
 
-            <Container style={{ marginBottom: "5rem" }}>
-                <div>
-                    {this.displayExercises(this.state.choices)}
-                </div>
-            </Container>
+            <>
+                <Container className="pt-5">
+                    <ExerciseTable />
+                </Container>
+            </>
 
         );
 
@@ -196,19 +58,115 @@ class Exercises extends Component {
 
 export default Exercises;
 
+const ExerciseTable = () => {
+    const [names, setNames] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [search, setSearch] = useState("");
+    const [sorting, setSorting] = useState({ field: "", order: "" });
 
-{/*<form onSubmit={this.submit}>*/}
+    const ITEMS_PER_PAGE = 2;
 
-{/*    <Form.Group>*/}
-{/*    <Form.Label>Exercise Name</Form.Label>*/}
-{/*    <Form.Control*/}
-{/*        placeholder="Exercise Name"*/}
-{/*        id="exerciseName"*/}
-{/*        as="textarea"*/}
-{/*        value={this.state.exerciseName}*/}
-{/*        rows={1}*/}
-{/*        onChange={this.handleChange}*/}
-{/*    />*/}
-{/*    </Form.Group>*/}
-{/*    <button>Submit</button>*/}
-{/*</form>*/}
+
+
+    useEffect(() => {
+        const getExercises = () => {
+            // showLoader();
+
+            axios.get('/api/exercises')
+                .then((response) => {
+                    const exercise = response.data
+                    // this.setState({ choices: exercise})
+                    setNames(exercise);
+                    console.log('Data has been received');
+                })
+                .catch(() => {
+                    alert('Error');
+                });
+        };
+
+        getExercises();
+    }, []);
+
+    const exerciseData = useMemo(() => {
+        let computedNames = names;
+
+        if (search) {
+            computedNames = computedNames.filter(
+                data =>
+                    data.exerciseName.toLowerCase().includes(search.toLowerCase()) ||
+                    data.exerciseType.toLowerCase().includes(search.toLowerCase())
+            );
+        }
+
+        setTotalItems(computedNames.length);
+
+        //Sorting comments
+        if (sorting.field) {
+            const reversed = sorting.order === "asc" ? 1 : -1;
+            computedNames = computedNames.sort(
+                (a, b) =>
+                    reversed * a[sorting.field].localeCompare(b[sorting.field])
+            );
+        }
+
+        //Current Page slice
+        return computedNames.slice(
+            (currentPage - 1) * ITEMS_PER_PAGE,
+            (currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+        );
+    }, [names, currentPage, search, sorting]);
+
+    return (
+        <>
+                <h3>Find Exercises Here</h3>
+                <div >
+                    <div >
+                        <div>
+                            <div>
+                                <PaginationComponent
+                                    total={totalItems}
+                                    itemsPerPage={ITEMS_PER_PAGE}
+                                    currentPage={currentPage}
+                                    onPageChange={page => setCurrentPage(page)}
+                                />
+                            </div>
+                            <div>
+                                <Search
+                                    onSearch={value => {
+                                        setSearch(value);
+                                        setCurrentPage(1);
+                                    }}
+                                />
+                            </div>
+                        </div>
+
+                        <Table striped bordered hover>
+                            <thead>
+                            <tr>
+                                <th>Exercise Name</th>
+                                <th>Exercise Type</th>
+                                <th>Equipment</th>
+                            </tr>
+                            {/*headers={headers}*/}
+                            {/*onSorting={(field, order) =>*/}
+                            {/*    setSorting({ field, order })*/}
+                            {/*}*/}
+                            </thead>
+                            <tbody>
+                            {exerciseData.map(name => (
+                                <tr key={name._id}>
+                                    <td>{name.exerciseName}</td>
+                                    <td>{name.exerciseType}</td>
+                                    <td>{name.equipment}</td>
+                                    {/*<td>{name.body}</td>*/}
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+        </>
+    );
+};
+
