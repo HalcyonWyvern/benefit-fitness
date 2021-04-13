c6466f6e7f1bb97908c4742ae4d0e77cf0d0cda3
HalcyonWyvern <cyclazocine@gmail.com>
2021-04-12 19:48:31 -0500

    Create component for creating empty workout plans.

---
+++ benefit-fitness/src/components/pages/admin_components/page_components/AddPlan.js
@@ -0,0 +1,151 @@
+import React, {Component} from "react";
+import axios from "axios";
+import {Button, Col, Form, Modal, Row} from "react-bootstrap";
+
+
+class AddExercise extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+            name: "",
+            trainerExplanation: "",
+            type: "",
+            exercises: []
+        }
+    }
+
+    showModal = () => {
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    hideModal = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+
+    valid = () => {
+        let nameErr = "";
+
+
+        if (!this.state.name) {
+            nameErr = "Please name the Plan."
+        }
+
+        if (nameErr) {
+            this.setState({
+                nameErr,
+            });
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const isValid = this.valid();
+        if(isValid) {
+            const newPlan = {
+                name: this.state.name,
+                trainerExplanation: this.state.trainerExplanation,
+                type: this.state.type,
+            }
+            console.log(newPlan);
+
+            axios.post("api/plans/", newPlan)
+                .then(res => {
+                    console.log(res.data);
+                })
+
+            this.setState({
+                name: "",
+                trainerExplanation: "",
+                type: "",
+                isOpen: false
+            })
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Button onClick={this.showModal}>
+                    New Plan
+                </Button>
+                <Modal size="lg" show={this.state.isOpen} onHide={this.hideModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create a new Plan.</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>This tool allows you to create empty workouts for the workout library. To add
+                        an exercise into newly created workouts, please use the appropriate tool.</p>
+                        <Form onSubmit={this.onSubmit}>
+                            <Form.Group>
+                                <Form.Label>Plan Name</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.name}
+                                    name="name"
+                                    id="name"
+                                    placeholder="Exercise Plan Name"
+                                    type="Box"
+                                />
+                            </Form.Group>
+                            <div style={{fontSize: 12, color: "red"}}>
+                                {this.state.nameErr}
+                            </div>
+                            <Form.Row>
+                                <h4>Optional Details</h4>
+                            </Form.Row>
+                            <Form.Group>
+                                <Form.Label>Plan Comments</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.trainerExplanation}
+                                    name="trainerExplanation"
+                                    id="trainerExplanation"
+                                    placeholder="You can explain the plan or tell users why you recommend it."
+                                    as="textarea"
+                                />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Plan Type</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.type}
+                                    name="type"
+                                    id="type"
+                                    as="select"
+                                >
+                                    <option>Please Select a Plan Type</option>
+                                    <option>Strength Building</option>
+                                    <option>Recreation</option>
+                                    <option>Health and Wellness</option>
+                                    <option>General Fitness</option>
+                                </Form.Control>
+                            </Form.Group>
+
+
+                            <p>{' '}</p>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+            </>
+        )
+    }
+}
+
+export default AddExercise;
--- benefit-fitness/src/components/pages/admin_components/pages/AdminPlans.js
+++ benefit-fitness/src/components/pages/admin_components/pages/AdminPlans.js
@@ -1,12 +1,18 @@
 import React, {Component} from "react";
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
+import AddPlan from "../page_components/AddPlan";
 
 class AdminPlans extends Component {
 
     render() {
         return (
             <Container>
-
+                <Row>
+                    <p>{' '}</p>
+                </Row>
+                <Row>
+                    <AddPlan/>
+                </Row>
             </Container>
         );
     }
