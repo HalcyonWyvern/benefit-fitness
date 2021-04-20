dbaa81995f1c48dffd9d3064d1abbc5bb0e047ac
HalcyonWyvern <cyclazocine@gmail.com>
2021-04-19 22:20:41 -0500

    Fixes ExerciseModal up with information.

--- benefit-fitness/src/components/pages/page_components/ExerciseModal.js
+++ benefit-fitness/src/components/pages/page_components/ExerciseModal.js
@@ -1,5 +1,7 @@
 import React from "react";
-import {Modal} from "react-bootstrap";
+import {Col, Container, Modal, Row} from "react-bootstrap";
+import ReactPlayer from 'react-player/lazy'
+
 
 
 const ExerciseModal = ({ exerciseData, showState, hideModal}) => {
@@ -9,8 +11,29 @@ const ExerciseModal = ({ exerciseData, showState, hideModal}) => {
                 <Modal.Title>{exerciseData.exerciseName}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p>There's nothing here just yet.</p>
-
+                <Container>
+                    <Row>
+                        <Col style={{paddingLeft: "14rem"}}>
+                            <ReactPlayer controls={true} url={exerciseData.videoURL}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={10}>
+                            <h5>Exercise Instructions</h5>
+                            <p>{exerciseData.instructions}</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h5>Exercise Type Classification</h5>
+                            <p>{exerciseData.exerciseType}</p>
+                        </Col>
+                        <Col>
+                            <h5>Required Equipment</h5>
+                            <p>{exerciseData.equipment}</p>
+                        </Col>
+                    </Row>
+                </Container>
             </Modal.Body>
         </Modal>
     )
