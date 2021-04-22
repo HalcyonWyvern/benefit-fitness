89aaca40fc957245c2bb4f83f428d1ae6b151ed4
HalcyonWyvern <cyclazocine@gmail.com>
2021-04-21 22:10:27 -0500

    Fixes minor styling issues that were covering the buttons.

--- benefit-fitness/src/components/auth_pages/Register.js
+++ benefit-fitness/src/components/auth_pages/Register.js
@@ -67,7 +67,7 @@ class Register extends Component {
         const { errors } = this.state;
 
         return (
-            <div className="container">
+            <div style={{paddingBottom: "15rem"}} className="container">
                 <div style={{ marginTop: "4rem" }} className="row">
                     <div className="col s8 offset-s2">
                         <Link to="/" className="btn-flat waves-effect">
