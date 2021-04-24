e8c68bbc24c3e8aeb3eda4189b2536a3b535bf6e
Nick <ntedesco627@gmail.com>
2021-04-24 11:49:27 -0500

    added sets, reps, time

--- server/models/workoutPlan.js
+++ server/models/workoutPlan.js
@@ -8,9 +8,33 @@ const planSchema = new Schema({
         index: true
     },
     exercises: [{
-            type: Schema.Types.ObjectId,
-            ref: "exercises",
-            required: true
+            type: "array",
+            // type: Schema.Types.ObjectId,
+            of: [
+                {
+                    title: "exerciseID",
+                    type: Schema.Types._ObjectId,
+                    ref: "exercises",
+                    required: true
+                },
+                {
+                    title: "sets",
+                    type: Number,
+                    required: false
+                },
+                {
+                    title: "reps",
+                    type: Number,
+                    required: false
+                },
+                {
+                    title: "time",
+                    type: Number,
+                    required: false
+                }
+            ]
+            // ref: "exercises",
+            // required: true
     }],
     trainerExplanation: {
         type: String,
