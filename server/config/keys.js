db15215fc40ef031bbb7d3c49648adb860829006
HalcyonWyvern <cyclazocine@gmail.com>
2021-05-10 15:36:52 -0500

    minor comment

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
+    //Repo is now public and old keys have been recycled. Use your env files, please.
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET
 }
